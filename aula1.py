7f9ef089b7460c411769594ebf110584d0527a04
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-08-24 23:04:54 -0300

    melhoria no código aula1.py

--- aula1.py
+++ aula1.py
@@ -1,3 +1,4 @@
+# Entrada de dados do usuário
 nome = input('Digite seu nome: ')
 idade = int(input('Digite sua idade: '))
 login = input('Digite seu login: ').lower()
@@ -5,58 +6,39 @@ senha = input('Digite sua senha: ')
 altura = float(input('Digite sua altura em metros: '))
 peso = float(input('Digite seu peso em KG: '))
 
-
+# Definindo critérios permitidos
 idade_permitida = 18
-senha_permitida = 12345678
 login_permitido = 'gabriel'
+senha_permitida = '12345678'
 
+# Função para exibir informações do usuário e tamanho da roupa
+def exibir_informacoes(nome, idade, altura, peso, tamanho):
+    print(f"""
+    Seu nome é {nome} e sua idade é {idade}
+    Seu nome tem {len(nome)} letras
+    Seu nome de trás pra frente é {nome[::-1]}
+    A primeira letra do seu nome é {nome[0]}
+    A última letra do seu nome é {nome[-1]}
+    Você veste tamanho {tamanho}
+    """)
+
+# Verificações de login, senha e idade
 if nome and idade:
     if idade >= idade_permitida:
-        if login == login_permitido and idade == idade_permitida:
+        if login == login_permitido and senha == senha_permitida:
             if altura <= 1.60 and peso <= 60:
-                print(f'Seu nome é {nome} e sua idade é {idade}')
-                print(f'seu nome tem len{nome} letras')
-                print(f'Seu nome de trás pra frente é {nome[::-1]}')
-                print(f'a primeira letra do seu nome é {nome[0]}') 
-                print(f'e a última letra do seu nome é {nome[-1]}')
-                print('Você veste tamanho P') 
+                exibir_informacoes(nome, idade, altura, peso, 'P')
             elif altura <= 1.70 and peso <= 70:
-                print(f'Seu nome é {nome} e sua idade é {idade}')
-                print(f'seu nome tem len{nome} letras')
-                print(f'Seu nome de trás pra frente é {nome[::-1]}')
-                print(f'a primeira letra do seu nome é {nome[0]}') 
-                print(f'e a última letra do seu nome é {nome[-1]}')
-                print('Você veste tamanho M')
-                
+                exibir_informacoes(nome, idade, altura, peso, 'M')
             elif altura <= 1.80 and peso <= 80:
-                print(f'Seu nome é {nome} e sua idade é {idade}')
-                print(f'seu nome tem len{nome} letras')
-                print(f'Seu nome de trás pra frente é {nome[::-1]}')
-                print(f'a primeira letra do seu nome é {nome[0]}') 
-                print(f'e a última letra do seu nome é {nome[-1]}')
-                print('Você veste tamanho g')
-            
+                exibir_informacoes(nome, idade, altura, peso, 'G')
             elif altura <= 1.90 and peso <= 90:
-                print(f'Seu nome é {nome} e sua idade é {idade}')
-                print(f'seu nome tem len{nome} letras')
-                print(f'Seu nome de trás pra frente é {nome[::-1]}')
-                print(f'a primeira letra do seu nome é {nome[0]}') 
-                print(f'e a última letra do seu nome é {nome[-1]}')
-                print('Você veste tamanho gg')
+                exibir_informacoes(nome, idade, altura, peso, 'GG')
             else:
                 print('Não encontramos seu tamanho')
         else:
-            print('Login e senha não permitidos')        
+            print('Login ou senha não permitidos')
     else:
-        print('Sua idade não é permitida')                
+        print('Sua idade não é permitida')
 else:
-    print('Você não digitou seu nome e idade')
-
-
-
-
-
-
-
-
-
+    print('Você não digitou seu nome ou idade')
