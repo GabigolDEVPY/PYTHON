7614b63fe5b916e923c9aaf179f68060eb973c50
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-08-31 22:15:04 -0300

    adicionando aula3 for and range

--- aula3.py
+++ aula3.py
@@ -1,8 +1,15 @@
-numeros = int(10000)
+numeros = 10000
 
 i = 0
 
 while i < numeros:
     print(f'{i} eu')
     i += 1
-    
+
+# usando o for e range 
+
+
+numeros = 10000
+
+for I in range (numeros):
+    print(f'{I} for ')
