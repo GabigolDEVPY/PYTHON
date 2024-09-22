cb6a7402c73da1af7397e3a9e9f1efab142bc06f
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-09-21 22:53:37 -0300

    modificação

--- aula13.py
+++ aula13.py
@@ -1,5 +1,5 @@
 lista = ['Gabriel', 'Cenoura', 'Comida']
-indices = range(len(lista))
+indices = range(len(lista)) #interact to indices list
 
 for indice in indices:
     print(indice, lista[indice], type(lista[indice]))
