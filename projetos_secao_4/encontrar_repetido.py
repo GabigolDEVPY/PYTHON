ffb381f22ed06ce283f524cb873e77b64b4dcf57
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-09-25 19:26:07 -0300

    encontrar repetido

--- projetos_secao_4/encontrar_repetido.py
+++ projetos_secao_4/encontrar_repetido.py
@@ -2,20 +2,15 @@ listas_produtos = [
     [1, 2, 3, 4, 5, 6, 7 , 7, 8, 9],
     [1, 4, 5, 3, 6, 7, 8, 5, 3, 4],
 ]
-numeros_checados = set()
-numero_duplicado = -1
+
+def encontrar_duplicado(lista):
+    numeros_checados = set()
+    for numero in lista:
+        if numero in numeros_checados:
+            return numero  # Retorna o primeiro duplicado encontrado
+        numeros_checados.add(numero)
+    return -1  # Retorna -1 se não houver duplicados
 
 for lista in listas_produtos:
-    for numero in lista:
-        if numero_duplicado != -1:
-            ...
-        else:
-            if numero in numeros_checados:
-                numero_duplicado = numero
-            else:
-                numeros_checados.add(numero)      
-
-
-        
-    print(f'{listas_produtos}{numero_duplicado}')    
-
+    numero_duplicado = encontrar_duplicado(lista)
+    print(f'Lista: {lista}, Número duplicado: {numero_duplicado}')
