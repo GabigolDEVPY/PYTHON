fd68f62e975473dfae4e99bfe00193b8456709a1
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-09-25 17:52:06 -0300

    praticando

---
+++ praticando.py
@@ -0,0 +1,25 @@
+idade = int(input('Digite sua idade: '))
+
+def somar(numero, numero2):
+        return numero + numero2
+        
+def inteiros(numero, numero3):
+    valor = numero + numero3
+    def somas2():
+        return valor + 4
+    
+    return somas2()
+        
+
+
+
+
+
+
+
+somar(2,5)
+inteiros(4,5)
+print(somar(2,5)) + (inteiros(4,5))
+
+if idade >= 18:
+    nome = input('Digite seu nome: ')
