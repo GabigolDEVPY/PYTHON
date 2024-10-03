912ed1e7a2a8d2a515567938ec5858710f960dbb
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-10-03 18:37:25 -0300

    aleatorio simulado

---
+++ aleatorios/simulado.py
@@ -0,0 +1,60 @@
+#QUESTÃO 1
+
+mesa_palmos_pai = 8
+mesa_palmos_ela = 11
+palmo_pai = 22
+
+mesa = mesa_palmos_pai * 22
+
+palmo_filha = mesa / 11
+
+#print(palmo_filha)
+
+
+
+#QUESTÃO 2
+
+
+soma_adicao = 2+3+0+8+9+9
+soma_multiplicacao = 
+
+
+print(soma_adicao)
+print(soma_multiplicacao)
+
+
+#QUESTÃO 3
+
+
+posicoes = "12345678"
+
+for i in range(2024):
+    print(posicoes[i])
+
+result = 2012 % 8
+
+print(result)
+
+
+#QUESTÃO 4
+
+Agosto =  3
+setembro =  2
+dia16 = 2
+segunda = 3
+terca = 2
+dia17 = 3
+
+Andrea = 3+2+3
+Daniela = 3+2+2
+Patricia = 3+3+3
+Fernanda = 2+3+2
+Tatiane = 2+3+3
+
+#QUESTÃO 5
+
+A = 8
+B = 4
+C = 6
+D = 2
+
