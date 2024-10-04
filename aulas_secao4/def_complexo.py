6766af48499dd7df5c8aceec54e787cbe3bfac9b
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-10-04 19:53:27 -0300

    fix

---
+++ aulas_secao4/def_complexo.py
@@ -0,0 +1,12 @@
+numero = int(input("DIgite um número: "))
+
+def funct(funcao):
+    resultado = 5 + funcao
+    return resultado
+
+def numeroo(numero):
+    soma = 5 + numero
+    return soma
+
+for i in range(funct(numeroo(numero))):
+    print(i)
