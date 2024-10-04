a3cec3962e34cfb1207d9b97dfd4032527a815af
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-10-04 20:02:43 -0300

    ADD arquivos

---
+++ aulas_secao4/ramdom.py
@@ -0,0 +1,11 @@
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+
+for indice, lsita in enumerate(lista):
+    print(f'{indice}, {lsita['nome']} {lsita['sobrenome']}')
