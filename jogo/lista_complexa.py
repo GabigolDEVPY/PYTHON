1c31a983a12ac0f4d042f10d5362af69861eafbf
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-10-05 16:11:24 -0300

    jogo

---
+++ jogo/lista_complexa.py
@@ -0,0 +1,9 @@
+dinheiro = 1000000
+
+lista_produtos = [
+    {"Carro": "Voyage", "Valor": "10000"},{"Carro": "Montana", "Valor": "9000"},{"Carro": "SallenS7", "Valor": "1000000"}
+    {"Carro": "Punto", "Valor": "12000"},{"Carro": "Cadete", "Valor": "8000"},{"Carro": "BMW", "Valor": "300000"}
+    {"Carro": "Golf", "Valor": "15000"},{"Carro": "Santana", "Valor": "12000"},{"Carro": "Jetta", "Valor": "150000"}
+    {"Carro": "Amarok", "Valor": "120000"},{"Carro": "Ferrari", "Valor": "1000000"},{"Carro": "JESKO", "Valor": "10000"}
+    {"Carro": "Civic", "Valor": "90000"},{"Carro": "Lamborghini", "Valor": "1200000"},{"Carro": "COROLA", "Valor": "10000"}
+]
