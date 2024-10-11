7a45300d396408d319f68aaff1823c456e799352
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-10-11 20:50:01 -0300

    Signed-off-by: Gabriel Rocha Dias <gabigolbr36@gmail.com>

--- AULAS/list_comprehensions.py
+++ AULAS/list_comprehensions.py
@@ -1,50 +1,50 @@
-# squares = []
+squares = []
 
-# for x in range(10):
-#     squares.append(x**2)
+for x in range(10):
+    squares.append(x**2)
 
-# print(squares)    
-
-
-
-
-# lista = [1, 2, 3, 4, 5, 6, 7, 8]
-
-# lista2 = [num * 2 for num in lista if num < 4]
-
-# print(lista2)
-
-
-# squares = list(map(lambda x: x**2, range(10)))
-
-# squares = [x**2 for x in range(10)]
-
-
-# lsitacom = [(x, y) for x in [1,2,3] for y in [3,1,4] if x !=y]
-
-
-# print(lsitacom)
-
-# combs = []
-# for x in [1,2,3]:
-#     for y in [3,1,4]:
-#         if x != y:
-#             combs.append((x, y))
-
-
-# def divisao(x, y):
-#     return x / y
+print(squares)    
 
 
 
-# divisaoo = [divisao(numero, 4) for numero in range(9)]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8]
+
+lista2 = [num * 2 for num in lista if num < 4]
+
+print(lista2)
 
 
-# print(divisaoo)
+squares = list(map(lambda x: x**2, range(10)))
 
-# impares = [numero for numero in range(1, 11) if numero % 2 != 0]
-# pares = [numero for numero in range(1, 11) if numero % 2 == 0 and numero > 4]
+squares = [x**2 for x in range(10)]
 
-# print(impares)
-# print(pares)
+
+lsitacom = [(x, y) for x in [1,2,3] for y in [3,1,4] if x !=y]
+
+
+print(lsitacom)
+
+combs = []
+for x in [1,2,3]:
+    for y in [3,1,4]:
+        if x != y:
+            combs.append((x, y))
+
+
+def divisao(x, y):
+    return x / y
+
+
+
+divisaoo = [divisao(numero, 4) for numero in range(9)]
+
+
+print(divisaoo)
+
+impares = [numero for numero in range(1, 11) if numero % 2 != 0]
+pares = [numero for numero in range(1, 11) if numero % 2 == 0 and numero > 4]
+
+print(impares)
+print(pares)
 
