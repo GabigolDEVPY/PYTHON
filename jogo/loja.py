78d23e099e0452d6ad74a6c24ea62ba33aafcb44
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-10-12 11:29:23 -0300

    Padronizando para item, complemento

--- jogo/loja.py
+++ jogo/loja.py
@@ -3,6 +3,12 @@ import json
 from shared import lista, meu_dinheiro, salvar_dinheiro
 
 
+def iterar_lista(lista):
+        for i, item in enumerate(lista):
+            print(f'{i}: {item['Periférico']}       Valor: {item['valor']}')
+            print()
+        comprar = input('Qual item deseja comprar [indice] ?: ')
+
 def lojaa():
     print(f'Meu dinheiro: {meu_dinheiro}')
     opcao = input('[1] Periféricos [2] Casas [3] Carros [4] sair\n'
@@ -11,11 +17,13 @@ def lojaa():
         return
     
     elif opcao == '1':
-        os.system('cls')
-        listap = lista[3]
-        for i, item in enumerate(listap):
-            print(f'{i}: {item['Periférico']}       Valor: {item['valor']}')
-            print()
+        iterar_lista(lista[3])
+    #     os.system('cls')
+    #     listap = lista[3]
+    #     for i, item in enumerate(listap):
+    #         print(f'{i}: {item['Periférico']}       Valor: {item['valor']}')
+    #         print()
+    #         comprar = input('Qual item deseja comprar [indice] ?: ')
 
     elif opcao == '2':
         os.system('cls')
@@ -23,6 +31,7 @@ def lojaa():
         for i, item in enumerate(listap):
             print(f'{i}: {item['Casa']}       Valor: {item['valor']}')
             print()
+            comprar = input('Qual item deseja comprar [indice] ?: ')
 
     elif opcao == '3':
         os.system('cls')
@@ -30,6 +39,7 @@ def lojaa():
         for i, item in enumerate(listap):
             print(f'{i}: {item['Carro']}       Valor: {item['Valor']}')
             print()
+            comprar = input('Qual item deseja comprar [indice] ?: ')
 
     elif opcao == '4':
         os.system('cls')
