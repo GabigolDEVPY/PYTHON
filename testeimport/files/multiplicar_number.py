34faca6f3e14b378b0238c7a273b65fb106dba9a
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-11-09 00:26:28 -0300

    teste import

---
+++ testeimport/files/multiplicar_number.py
@@ -0,0 +1,7 @@
+import random
+
+
+def multi_number(x):
+    return [random.randint(0, 6) * x // 4 for num in range(10)]
+
+
---
+++ testeimport/main.py
@@ -0,0 +1,9 @@
+from files.multiplicar_number import multi_number
+
+
+number = int(input("Type a number: "))
+
+result = multi_number(number)
+
+
+print(result)
