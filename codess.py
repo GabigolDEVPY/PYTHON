532be2f6dfb36a986dd1938f4bbc3e123a49a073
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-11-11 20:30:54 -0300

    add files

--- codess.py
+++ codess.py
@@ -1,5 +1,46 @@
 import sys
 
-print("este múdulo se chama", __name__)
+# print(*sys.path, sep='\n')
 
-print(*sys.path, sep='\n')
+lista = [{"nome": "gabigol", "sobrenome": "brasil"},
+        {"nome": "babigolbr", "sobrenome": "europa"},
+        {"nome": "dark", "sobrenome": "eua"}
+        ]
+
+ordened_list = sorted(lista, key=lambda x: x['nome'])
+print(ordened_list)
+
+
+numeros = [1, 3, 5, 4, 2]
+
+
+
+new_numbers = sorted(numeros)
+new_numbers = sorted(numeros, reverse=True)
+
+nomes = ["Ana", "Bruno", "Carla", "Davi"]
+
+ordened_names = sorted(nomes, key=len)
+
+print(new_numbers)
+print(ordened_names)
+
+
+pessoas = [
+    {"nome": "Gabriel", "idade": 25},
+    {"nome": "Ana", "idade": 30},
+    {"nome": "Bruno", "idade": 20}
+]
+
+ordened_peoples = sorted(pessoas, key=lambda x: x['idade'])
+
+print(ordened_peoples)
+
+
+def obtain_people(people):
+    return people["idade"]
+
+
+ordenados = sorted(pessoas, key=obtain_people)
+
+print(ordenados)
