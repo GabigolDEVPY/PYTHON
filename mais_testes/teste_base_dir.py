8d57a9b14accdf87d88c15dec0ad7638f4843e6b
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-12-16 23:45:26 -0300

    add files

---
+++ mais_testes/teste_base_dir.py
@@ -0,0 +1,41 @@
+import json 
+import os
+
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+json_file = os.path.join(base_dir, "dados.json")
+
+
+def load_file():
+    try:
+        with open(json_file, "r") as f:
+            return json.load(f)
+    except:
+        (FileExistsError, FileNotFoundError)
+        print('File not found or exists')
+        return []
+
+def save_file(lista):
+    with open(json_file, "w") as f:
+        json.dump(lista, f, indent=4)
+    
+
+
+
+def add_client():
+    users = load_file()
+    nome = input("Type client name: ")
+    cpf = input("Type client cpf: ")
+    
+    add_client = {
+        "Nome": nome,
+        "CPF": cpf 
+    }
+    users.append(add_client)
+    save_file(users)
+
+
+while True:
+    add_client()
+
+
