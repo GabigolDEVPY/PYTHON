eb6302cfc8d18905b6b57bc0e6cdd3c66a72dcca
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2024-12-24 10:58:06 -0300

    add files

--- tela_teste/backend/teste_base_dir.py
+++ tela_teste/backend/teste_base_dir.py
@@ -1,24 +1,33 @@
 import json 
 import os
+import sys
 
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
-json_file = os.path.join(base_dir, "dados.json")
+user = os.getlogin()
+
+base_dir = os.path.join("C:\\Users", user, "Documents", "Register Program")
+json_file = os.path.join(base_dir,"dados.json")
+os.makedirs(base_dir, exist_ok=False)
+
+# print(base_dir)  
+print(json_file)  
+
+
 
 
 def load_file():
     try:
         with open(json_file, "r") as f:
             return json.load(f)
-    except:
-        (FileExistsError, FileNotFoundError)
+    except (FileExistsError, FileNotFoundError):
         print('File not found or exists')
         return []
 
 def save_file(lista):
     with open(json_file, "w") as f:
         json.dump(lista, f, indent=4)
-    
+
+
 
 
 
