5d2a7eedcc9cc7991c65cc69f924bd0827fd5ef7
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-01-03 11:35:34 -0300

    add files

--- AULAS/3em1_exercicios.py
+++ AULAS/3em1_exercicios.py
@@ -10,7 +10,7 @@ produtos = [
 
 # obs: SEU MORANGO GOSTARIA DE UMA LISTA MAIS ORGANIZADA
 lista_produtos = [{"nome": produto["nome"], "preco": produto["preco"]} for produto in produtos]
-# print(lista_produtos)
+print(lista_produtos)
 
 def aumenta_preco(lista):
     novos_produtos = lista.copy()
@@ -20,12 +20,18 @@ def aumenta_preco(lista):
     novos_produtos.sort(key=lambda produto: produto["preco"])
     produtos_por_nome.sort(key=lambda produto: produto["nome"])
     produtos_por_nome.reverse()
-    # print(novos_produtos)
-    # print(produtos_por_nome)
+    print(novos_produtos)
+    print(produtos_por_nome)
     return produtos_por_nome
 
 
-# aumenta_preco(produtos)
+aumenta_preco(produtos)
 
 copia_profunda = copy.deepcopy(aumenta_preco(lista_produtos))
-print(copia_profunda)
+print(*copia_profunda, sep="\n")
+
+
+outra = [{**p, 'preco': round(p['preco'] * 1.1, 2)} for p in copy.deepcopy(produtos)]
+outra = [{**p, 'preco': round(p['preco'] * 1.1, 2)} for p in copy.deepcopy(produtos)]
+outra = [{**p, 'preco': round(p['preco'] * 1.1, 2)} for p in copy.deepcopy(produtos)]
+print(*outra, sep="\n")
