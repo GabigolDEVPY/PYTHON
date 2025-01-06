8dee65e4eacfd388ec7e1c39f0d58b46eed9c3ce
gabriel rocha dias <gabigolbr36@gmail.com>
2025-01-06 15:14:21 -0300

    add files

--- hacker_rank/hacker_rank9.py
+++ hacker_rank/hacker_rank9.py
@@ -1,17 +1,18 @@
-n = int(input("Number of students: "))
+n = int(input())
 
 list_students = []
 notas = set()
 for student in range(n):
-    name = input("enter student name: ")
-    notice = float(input("enter student notice: "))
+    name = input()
+    notice = float(input())
     notas.add(notice)
     list_students.append([name, notice])
 notas_ordenadas = sorted(notas)
 students_com_notas = []
 for student in list_students:
-    if notas_ordenadas[1] == student[1]:
+    if notas_ordenadas[1] == student[1] and len(notas_ordenadas) > 1:
         students_com_notas.append(student[0])
-        print(student[0])
 students_com_notas.sort()
+for student in students_com_notas:
+    print(student)
 
