c1f37d044f122dba568bde7c899e57f777425a8a
gabriel rocha dias <gabigolbr36@gmail.com>
2025-01-06 16:33:05 -0300

    add files

---
+++ variaveis_livres.py
@@ -0,0 +1,16 @@
+def concatenar(string_inicial):
+    valor_final = string_inicial
+
+    def interna(valor_a_concatenar=''):
+        nonlocal valor_final
+        valor_final += valor_a_concatenar
+        return valor_final
+    return interna
+
+
+c = concatenar('a')
+print(c('b'))
+print(c('c'))
+print(c('d'))
+final = c()
+print(final)
