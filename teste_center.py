c447a1be2426914ec7a864dd1a072df886251f85
gabriel rocha dias <gabigolbr36@gmail.com>
2025-01-08 13:58:35 -0300

    add files

---
+++ teste_center.py
@@ -0,0 +1,8 @@
+def door(n, m):
+    for i in range(1, n, 2):
+        print((".|." * i).center(m, "-"))
+    print("WELCOME".center(m, "-"))
+    for i in range(n-2, 0, -2):
+        print((".|." * i).center(m, "-"))
+n, m = map(int, input().split())
+door(n, m)
