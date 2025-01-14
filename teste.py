7a955d8e656dae0729ee25c7338bd946d6d5756b
gabriel rocha dias <gabigolbr36@gmail.com>
2025-01-13 21:22:55 -0300

    add files

--- teste.py
+++ teste.py
@@ -1,3 +1,12 @@
 lista1, lista2 = [list(map(int, parte.split())) for parte in input("Digite as listas separadas por '-': ").split("-")]
 print(lista1, lista2)
 
+def function(lista):
+    new_list = ["gabriel" + str(((num * 12) - 12) ** 12 - 100) for num in lista if num % 2 == 0] 
+    print(new_list)
+
+function(lista1)
+
+
+
+    
