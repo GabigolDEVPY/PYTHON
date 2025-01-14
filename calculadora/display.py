194c320c3e6cbd798e3d6be1da2ae2c8f5f7285a
gabriel rocha dias <gabigolbr36@gmail.com>
2025-01-14 16:53:15 -0300

    add files

--- calculadora/display.py
+++ calculadora/display.py
@@ -5,6 +5,7 @@ from PySide6.QtGui import *
 class Display(QLineEdit):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
         super().__init__(parent, *args, **kwargs)
+        self.configStyle()
     
     def configStyle(self):
-        self.setStyleSheet("font-size: 30px")
+        self.setStyleSheet("font-size: 30px;")
