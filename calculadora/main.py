611a284b55cdb313cb7e5beda200840edfe90797
gabriel rocha dias <gabigolbr36@gmail.com>
2025-01-14 21:37:40 -0300

    add files

--- calculadora/main.py
+++ calculadora/main.py
@@ -6,12 +6,13 @@ from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from variables import WINDOW_ICON_PATH
+from styles import setupTheme
 
 if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
-
+    setupTheme()
     # Define o ícone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
---
+++ calculadora/styles.py
@@ -0,0 +1,4 @@
+import qdarktheme 
+
+def setupTheme():
+    qdarktheme.setup_theme()
