8875215b25e571b0dae941e24b3f1c429162504e
gabriel rocha dias <gabigolbr36@gmail.com>
2025-01-16 13:55:31 -0300

    add files

--- server.py
+++ server.py
@@ -4,16 +4,15 @@ app = Flask(__name__)
 
 dados = {
     "Gabriel": "sao paulo",
-    "Gabriela": "sao paulo",
+    "Gabriela": ["sao pauloooooo"],
     "ramdom": "sao paulo"
 }
 lista = ["gabriel", "eu"]
 
 @app.route("/buscar/nome=<nome>", methods= ["GET"])
 def buscar(nome):
-    if nome in dados:
-        return lista
-    
+    return dados.get(nome, "USUÁRIO NÃO ENCONTRADO")
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')    
     
---
+++ testando_get.py
@@ -0,0 +1,10 @@
+dados = {
+    "nome": "gabriel",
+    "nome2": "gabigol",
+    "nome3": "gabigordo"
+    }
+
+name = "nome4"
+result = dados.get(name, "não encontrado")
+
+print ( result ) 
