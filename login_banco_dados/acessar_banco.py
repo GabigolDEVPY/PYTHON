f304e6bd33192acdda6814306260f22c98874af5
gabriel rocha dias <gabigolbr36@gmail.com>
2025-01-17 12:27:11 -0300

    add files

---
+++ login_banco_dados/acessar_banco.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+
+local = os.path.dirname(__file__)
+dir_dados = os.path.join(local, "dados.json")
+
+
+def load_data():
+    try:
+        with open(dir_dados, "r") as f:
+            return json.load(f)
+        
+    except:
+        return {}
+    
+def save_data(dados):
+    try:
+        with open(dir_dados, "w") as f:
+            return json.dump(dados, f)
+        
+    except:
+        return "Não foi possível conectar ao banco de dados"
+    
