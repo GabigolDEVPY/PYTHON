1e8c1e60d2df82cda00077b713f7c9e5ae64313b
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-06-04 21:00:32 -0300

    add files

--- TODOS/aprendendo_classes/classes1.py
+++ TODOS/aprendendo_classes/classes1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Pessoa():
     def __init__(self, nome, sobrenome, idade, peso):
         self.nome = nome
@@ -12,19 +11,4 @@ class Pessoa():
 
 pessoa1 = Pessoa("gabriel", "rocha", 18, 79)
 
-=======
-class Pessoa():
-    def __init__(self, nome, sobrenome, idade, peso):
-        self.nome = nome
-        self.sobrenome = sobrenome
-        self.idade = idade
-        self.peso = peso
-        
-    def deixa_maiusculo(self):
-        return self.nome.upper()
-
-
-pessoa1 = Pessoa("gabriel", "rocha", 18, 79)
-
->>>>>>> 6b5cb26 (add files corrupted)
 print(pessoa1.deixa_maiusculo())
