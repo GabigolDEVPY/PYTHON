4d97a3991e27bb65c341e71e633affd98df8ca5f
Gabriel rocha dias <gabigolbr36@gmail.com>
2025-08-01 00:40:39 -0300

    add files

--- TODOS/AULAS/aprendendo_classes/classe10.py
+++ TODOS/AULAS/aprendendo_classes/classe10.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "amor de morango 123"
 
 clientes = [
     {"id": 1, "nome": "gabriel", "idade": 20,  "login": "gabgab", "senha": "12345678", "value": 1000},
@@ -11,50 +12,25 @@ class Pessoa():
         self.nome = nome
         self.idade = idade
 
-    def falar_idade(self):
-        print(self.idade)
-
-
-class Client():
-    def __init__(self, id):
-        for cliente in clientes:
-            if id == cliente["id"]:
-                self.nome, 
-            
-    class Pessoa():
-        def __init__(self, nome, idade):
-            self.nome = nome
-            self.idade = idade
-
-    def __init__(self, id,  nome, idade, login, senha, value):
+class Client(Pessoa):
+    def __init__(self,  nome, idade, login, senha, value, id):
         super().__init__(nome, idade)
         self.login = login
         self.senha = senha
         self.value = value
         self.id = id
 
-
-@app.route("/login2", methods=["POST"])
-def return_user2():
-    dados = request.get_json()
-    print(dados)
+def return_cliente(id_cliente):
     for cliente in clientes:
-        if dados['id'] == cliente["id"]:
+        if id_cliente['id'] == cliente["id"]:
+            session['id'] = cliente['id']
             cliente = Client(**cliente)
-            return f"Esse é o cliente: {cliente.nome}, {cliente.value}" 
-    return "error"    
+            return cliente
 
-app.run(debug=True)    
+@app.route("/login", methods=["POST"])
+def return_user():
+    id_cliente = request.get_json()
+    cliente = return_cliente(id_cliente)
+    return (cliente.__dict__)
 
-
-class Carro:
-    class Motor:
-        def __init__(self, potencia):
-            self.potencia = potencia
-
-    def __init__(self, modelo, potencia_motor):
-        self.modelo = modelo
-        self.motor = Carro.Motor(potencia_motor)
-
-carro = Carro("Fusca", 50)
-print(carro.motor.potencia)  # 50
+app.run(debug=True, port=5020, host='0.0.0.0')    
